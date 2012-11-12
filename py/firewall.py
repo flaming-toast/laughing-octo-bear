582a520cf56b394fba0cca19c228bad9fa86799e
jason-berkeley <jason@server>
2012-11-12 15:45:41 -0800

    #2 completed, moving to #3

--- py/firewall.py
+++ py/firewall.py
@@ -2,6 +2,7 @@ from pox.core import core
 from pox.lib.addresses import * 
 from pox.lib.packet import *
 import fileinput
+import re
 # Get a logger
 log = core.getLogger("fw")
 
@@ -17,26 +18,39 @@ class Firewall (object):
     Constructor.
     Put your initialization code here.
     """
-    self.banned_ports = {}
+    self.banned_ports = set()
     for line in fileinput.input('/root/pox/ext/banned-ports.txt'):
-      portNumber = int(line)
-      self.banned_ports[portNumber] = True
-    
+        portNumber = int(line.rstrip())
+        log.debug(portNumber)
+        self.banned_ports.add(portNumber)
+    self.banned_domains = set()
+    for line in fileinput.input('/root/pox/ext/banned-domains.txt'):
+        domain = str(line.rstrip()) # Return a copy of the string with trailing characters removed. 
+        log.debug(domain)
+        self.banned_domains.add(domain)
+        
     log.debug("Firewall initialized.")
-
+    
   def _handle_ConnectionIn (self, event, flow, packet):
     """
     New connection event handler.
     You can alter what happens with the connection by altering the
     action property of the event.
     """
-    if self.banned_ports[flow.dstport] == True:
-        log.debug("Banned Ports- packet dropped")
+    if flow.dstport in self.banned_ports:
+        log.debug("Denied Connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
         event.action.deny = True
-    
-    
+        return
+    if flow.dstport == 80:
+        log.debug("Deferred connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+        event.action.defer = True
+        return
+        
+        
+      
     
     log.debug("Allowed connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+    event.action.forward = True
   def _handle_DeferredConnectionIn (self, event, flow, packet):
     """
     Deferred connection event handler.
@@ -44,7 +58,22 @@ class Firewall (object):
     handler will be called when the first actual payload data
     comes across the connection.
     """
-    pass
+    forward = True
+    log.debug("handle deferred connection")
+    user=re.compile("Host: (.*?)\r") # consulted from http://stackoverflow.com/questions/10832974/python-regular-expression-for-http-request-header
+    host = user.findall(packet.payload.payload.payload)
+    if (host[0] in self.banned_domains):
+        log.debug(host[0])
+        forward = False
+    for banned_domains in self.banned_domains:
+        if banned_domains in host[0]:
+            log.debug(host[0])
+            forward = False 
+    if forward:
+        event.action.forward = True
+    else:
+        event.action.forward = False
+    
     
   def _handle_MonitorData (self, event, packet, reverse):
     """
